0e6328039b81ab1736d951a6bb1553aa54f6e6e2
Koras02 <wjdgh0727@gmail.com>
2021-08-24 00:39:23 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+// react axios 를 사용한 예외처리 방법
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+         예외 처리방버임
+      </>
   );
 }
 
